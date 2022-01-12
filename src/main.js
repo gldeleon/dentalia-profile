47e95a57c7875dd1081fbe8fad2016901cc8a89f
Leonel De Leon <gldeleon@live.com.mx>
2022-01-11 22:14:22 -0600

    fix: api url

--- src/main.js
+++ src/main.js
@@ -12,6 +12,9 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../src/assets/global-css/adminlte.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 
 Vue.use(VueSweetalert2);
 Vue.use(VueSession)
@@ -20,11 +23,27 @@ Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios;
-Vue.prototype.$url = "http://localhost:9001/api/";
-//Vue.prototype.$url = process.env.VUE_APP_API_URL;
+// Vue.prototype.$url = "http://localhost:9001/api/";
+// Vue.prototype.$url = "http://apiperfiles.dentalia.com.mx/api/";
+Vue.prototype.$url = process.env.VUE_APP_API_URL;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvcralV2a86SmVTu3QLyhMf65_JhUvnQo",
+  authDomain: "mis-pwa.firebaseapp.com",
+  databaseURL: "https://mis-pwa.firebaseio.com",
+  projectId: "mis-pwa",
+  storageBucket: "mis-pwa.appspot.com",
+  messagingSenderId: "262062419750",
+  appId: "1:262062419750:web:42b8088c5c2ca19ac31be7",
+  measurementId: "G-F0KX8TPQ2H"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 new Vue({
   router,
   store,
+  analytics,
   render: h => h(App)
 }).$mount('#app')
